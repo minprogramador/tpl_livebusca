dccd253052663b7cbdb0a7907ed87c2664bb088c
minprogramador <minprogramador@gmail.com>
2019-02-01 15:55:46 -0200

    fonts

--- js/app.js
+++ js/app.js
@@ -21,9 +21,14 @@ function loadmain() {
 		$.get('js/tpls/login.html', function(template) {
 			var rendered = Mustache.render(template);
 			$('#boxmain').html(rendered);
+			//onload();
+
+
+
 		});
   });
 }
+
 
 function loadFaleConosco() {
 	$.get('js/tpls/contato.html', function(template) {
@@ -38,3 +43,23 @@ function loadEsqueceuSenha() {
 		$('#boxmain').html(rendered);
 	});
 }
+
+
+// function onSubmit(token) {
+//     alert('Thanks ' + document.getElementById('usuario').value + '!');
+//   }
+
+//   function validate(event) {
+//     event.preventDefault();
+//     if (!document.getElementById('usuario').value) {
+//       alert("Please enter your name.");
+//     } else {
+//       grecaptcha.execute();
+//     }
+//   }
+
+//   function onload() {
+//     var element = document.getElementById('submit');
+//     element.onclick = validate;
+//   
+
